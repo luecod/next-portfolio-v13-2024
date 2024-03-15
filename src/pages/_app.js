a850c3925de752807fab026073f56f5f52062965
Luis Valladolid <eduvalladolid1997@gmail.com>
2024-03-15 00:56:35 -0500

    no se que estoy haciendo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,22 @@
+import Head from 'next/head'
 import '@/styles/globals.css'
 import { Montserrat } from "next/font/google"
 
 
+
 const montserrat = Montserrat({
   subsets: ['latin'],
-  display: 'swap',
   variable: '--font-mont',
 })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head><meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" /></Head>
+
+      <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+        <Component {...pageProps} />
+      </main></>
+  )
 }
